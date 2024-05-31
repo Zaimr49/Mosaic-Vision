60778f9142a8892e2a1c1b2d25f93586d40cd00f
Fayzan Ali Akhtar <fayzan585@gmail.com>
2024-06-01 02:07:11 +0500

    Backend connected to DB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,27 @@
-// src/index.ts
-const greet = (name: string): string => {
-    return `Hello, ${name}!`;
-  };
-  
-  console.log(greet("World"));
+import express, { Application } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './DB/Connect'; 
 
-  
+dotenv.config();
+
+const app: Application = express();
+const port: string | number = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.use(express.json());
+
+const start = async (): Promise<void> => {
+  try {
+    await connectDB(process.env.MONGO_URI as string);
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}...`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+start();
