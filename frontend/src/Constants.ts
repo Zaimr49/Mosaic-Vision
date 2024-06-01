bfd6774844231247eefb555fb1c485170617bfc1
Fayzan Ali Akhtar <fayzan585@gmail.com>
2024-06-01 14:45:42 +0500

    connecting backend to frontend

--- frontend/src/Constants.ts
+++ frontend/src/Constants.ts
@@ -1,1 +1,2 @@
-export const testAPI = 'http://localhost:5000/api/connection/test'
+export const base_url = process.env.REACT_APP_DEPLOYED_BACKEND_URL || 'http://localhost:5000'
+export const testAPI = `${base_url}/api/connection/test`
