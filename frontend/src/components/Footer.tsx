981814d9e4c0fe94cf1554cfc03814ef45f30e22
Mustafa-Ashfaq81 <mustafaaplus52@gmail.com>
2024-06-02 01:34:50 +0500

    Footer style final

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -9,50 +9,50 @@ const Footer: React.FC = () => {
       <Container maxWidth="lg">
         <Grid container spacing={4}>
           <Grid item xs={12} sm={3}>
-            <Typography variant="h6" gutterBottom>
+            <Typography variant="h6" gutterBottom sx={{ fontWeight: 'bold', color: 'grey.700' }}>
               Catalog
             </Typography>
-            <Link href="#" color="inherit" underline="none" display="block">Labels</Link>
-            <Link href="#" color="inherit" underline="none" display="block">Hang Tags</Link>
-            <Link href="#" color="inherit" underline="none" display="block">Inserts</Link>
-            <Link href="#" color="inherit" underline="none" display="block">Stickers</Link>
-            <Link href="#" color="inherit" underline="none" display="block">PU Patches</Link>
-            <Link href="#" color="inherit" underline="none" display="block">Office Stationery</Link>
-            <Link href="#" color="inherit" underline="none" display="block">Promotional Items</Link>
+            <Link href="#" color="inherit" underline="none" display="block" sx={{ color: 'grey.600', mb: 1 }}>Labels</Link>
+            <Link href="#" color="inherit" underline="none" display="block" sx={{ color: 'grey.600', mb: 1 }}>Hang Tags</Link>
+            <Link href="#" color="inherit" underline="none" display="block" sx={{ color: 'grey.600', mb: 1 }}>Inserts</Link>
+            <Link href="#" color="inherit" underline="none" display="block" sx={{ color: 'grey.600', mb: 1 }}>Stickers</Link>
+            <Link href="#" color="inherit" underline="none" display="block" sx={{ color: 'grey.600', mb: 1 }}>PU Patches</Link>
+            <Link href="#" color="inherit" underline="none" display="block" sx={{ color: 'grey.600', mb: 1 }}>Office Stationery</Link>
+            <Link href="#" color="inherit" underline="none" display="block" sx={{ color: 'grey.600', mb: 1 }}>Promotional Items</Link>
           </Grid>
           <Grid item xs={12} sm={3}>
-            <Typography variant="h6" gutterBottom>
+            <Typography variant="h6" gutterBottom sx={{ fontWeight: 'bold', color: 'grey.700' }}>
               Information
             </Typography>
-            <Link href="#" color="inherit" underline="none" display="block">FAQs</Link>
-            <Link href="#" color="inherit" underline="none" display="block">Products</Link>
-            <Link href="#" color="inherit" underline="none" display="block">Services</Link>
-            <Link href="#" color="inherit" underline="none" display="block">About Us</Link>
+            <Link href="#" color="inherit" underline="none" display="block" sx={{ color: 'grey.600', mb: 1 }}>FAQs</Link>
+            <Link href="#" color="inherit" underline="none" display="block" sx={{ color: 'grey.600', mb: 1 }}>Products</Link>
+            <Link href="#" color="inherit" underline="none" display="block" sx={{ color: 'grey.600', mb: 1 }}>Services</Link>
+            <Link href="#" color="inherit" underline="none" display="block" sx={{ color: 'grey.600', mb: 1 }}>About Us</Link>
           </Grid>
           <Grid item xs={12} sm={3}>
-            <Typography variant="h6" gutterBottom>
+            <Typography variant="h6" gutterBottom sx={{ fontWeight: 'bold', color: 'grey.700' }}>
               Our Store
             </Typography>
-            <Link href="#" color="inherit" underline="none" display="block">About Mosaic Vision</Link>
-            <Link href="#" color="inherit" underline="none" display="block">Contact</Link>
-            <Link href="#" color="inherit" underline="none" display="block">Terms of Use</Link>
+            <Link href="#" color="inherit" underline="none" display="block" sx={{ color: 'grey.600', mb: 1 }}>About Mosaic Vision</Link>
+            <Link href="#" color="inherit" underline="none" display="block" sx={{ color: 'grey.600', mb: 1 }}>Contact</Link>
+            <Link href="#" color="inherit" underline="none" display="block" sx={{ color: 'grey.600', mb: 1 }}>Terms of Use</Link>
           </Grid>
           <Grid item xs={12} sm={3}>
-            <Typography variant="h6" gutterBottom>
+            <Typography variant="h6" gutterBottom sx={{ fontWeight: 'bold', color: 'grey.700' }}>
               Contact
             </Typography>
             <Typography variant="body2" color="textSecondary">
               19-A Abbot Road Near PTV Station, Opp Daily Sadaat Lahore Pakistan<br />
-              Daily 9:00 am – 10:00 pm<br />
-              +92 324 8473731<br />
-              Toll free
+              Mon-Sat 9:00 am - 10:00 pm<br />
+              <br />
+              Tel: +92 324 8473731<br />
             </Typography>
             <Box mt={2}>
               <Link href="https://www.facebook.com/mosaicvisionpk/" color="inherit" sx={{ mr: 1 }}>
-                <FacebookIcon />
+                <FacebookIcon sx={{ fontSize: 40 }} />
               </Link>
               <Link href="https://www.linkedin.com/company/mosaic-vision/" color="inherit">
-                <LinkedInIcon />
+                <LinkedInIcon sx={{ fontSize: 40 }} />
               </Link>
             </Box>
           </Grid>
